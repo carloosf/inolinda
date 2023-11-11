d177fd82cc95775b61678a77fb8ce1aa475de7c8
Carlos Silva <88803987+carloosf@users.noreply.github.com>
2023-11-11 20:49:54 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import Hero from "./components/hero/Hero";
-import "./App.css"; 
+import ScreenNovel from "./components/screenNovel/ScreenNovel";
+import "./App.css";
 
 export default function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([{}]);
   const [showHero, setShowHero] = useState(true);
+  const [step, setStep] = useState(0);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -12,17 +14,32 @@ export default function App() {
     }, 3000);
 
     return () => clearTimeout(timeoutId);
-  }, []); 
+  }, []);
+
+  const handleClick = () => {
+    setStep(step + 1);
+  };
 
   return (
     <div className="container">
-      <div>
-        
-      </div>
-    
       {showHero && <Hero />}
-      
-      <div></div>
+      {!showHero && step === 0 && (
+        <div className="containerTextArea">
+          <div className="img"></div>
+          <div className="textArea">
+            <p>
+              Bem vindo a RECCITY, uma vanguarda entre as Smartcities do século
+              XXII. No ano de 2117, esta metrópole futurista destaca-se, onde
+              cada cidadão é um especialista em sua área.
+            </p>
+            <button onClick={handleClick}>Proximo</button>
+          </div>
+        </div>
+      )}
+      {!showHero ||
+        (step === 1 && (
+          <ScreenNovel question="Qual é o seu nome?" responses={[]} />
+        ))}
     </div>
   );
 }
--- src/components/screenNovel/ScreenNovel.tsx
+++ src/components/screenNovel/ScreenNovel.tsx
@@ -2,16 +2,20 @@ import "./ScreenNovel.css";
 
 interface PropsScreenNovel {
   question: string;
-  response: string[];
+  responses: string[]; // Alterei o tipo de 'response' para 'responses', assumindo que é um array de strings
 }
 
-export default function ScreenNovel({ question, response }: PropsScreenNovel) {
+export default function ScreenNovel({ question, responses }: PropsScreenNovel) {
   return (
     <div className="containerSN">
-      <div className="imgBG" />
+      <div className="imgBG"></div>
       <div className="textArea">
         <p>{question}</p>
-        <button>{response}</button>
+        <div className="responseButtons">
+          {responses.map((response, index) => (
+            <button key={index}>{response}</button>
+          ))}
+        </div>
       </div>
     </div>
   );
